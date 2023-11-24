38bd580957c2134481ce00662b61472b6c04ee8f
egblanco <egblanco.betasoft@gmail.com>
2023-11-23 18:55:03 -0600

    Integrating image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["open-ai-file.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "open-ai-file.s3.amazonaws.com",
+      },
+    ],
   },
 };
 
