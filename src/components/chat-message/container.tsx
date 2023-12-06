a7064970e440dcbf81bd6418e62f62a1e11370d5
egblanco <egblanco.betasoft@gmail.com>
2023-12-06 12:18:43 -0600

    referenes

--- src/components/chat-message/container.tsx
+++ src/components/chat-message/container.tsx
@@ -3,7 +3,7 @@ import useEditMessages from "@/contexts/chat-messages";
 import { v4 } from "uuid";
 import { Message } from "@/types/message";
 import MessageBox from "@/components/chat-message/message";
-import MessageBoxStatus from "./message-box-status";
+import MessageBoxStatus from "@/components/chat-message/message-box-status";
 import { TypeAnimation } from "react-type-animation";
 import { conversation } from "@/services/openai";
 import { USER_TYPE } from "@/constants/common";
